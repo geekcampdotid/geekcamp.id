7a5c2d612e7adb47e9a5ad1ebc77c29c5112a19f
Tommy Sullivan <tommy.b.sullivan@gmail.com>
2017-06-11 23:51:34 +0700

    remove the CFP flow, just the form

--- spec/features/cfp_flow_spec.rb
+++ spec/features/cfp_flow_spec.rb
@@ -4,11 +4,7 @@ feature "CFP Flow" do
 
   scenario "Visitors can navigate to the CFP page" do
     visit home_path
-
-    click_link "Submit A Talk"
-    expect(page.current_path).to eq(cfp_path)
-
-    click_link "Submit your talk"
+    click_link "CFP"
     expect(page.current_path).to eq(new_cfp_path)
   end
 

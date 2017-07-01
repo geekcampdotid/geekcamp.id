3f32520720e64b65e9766a2c309b87f90390c260
kusut <tinokusut@gmail.com>
2017-07-01 20:35:17 +0700

    Update transactions.py

--- transactions.py
+++ transactions.py
@@ -7,19 +7,27 @@ import csv
 
 bl = []
 ours = []
+bl_total = 0
 
 with open('bl.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile)
-    for row in spamreader:
-        bl.append(row[1])
+    lines = csv.reader(csvfile)
+
+    for i, row in enumerate(lines):
+        if i:
+            bl.append(row[1])
+            bl_total += int(row[18])
 
 
 with open('ours.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile)
-    for row in spamreader:
+    lines = csv.reader(csvfile)
+
+    for row in lines:
         if row[2]:
-            ours.append('170' + row[2].rsplit('/', 1)[-1])
+            ours.append('170' + row[2].rsplit('/')[-1])
 
 
 delta = set(bl) - set(ours)
-print(delta)
+print("BL tickets count: " + str(bl_total))
+print("Transactions without form data: ")
+for trx in delta:
+    print(trx)

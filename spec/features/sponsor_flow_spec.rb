7f0816c0aa64483de2e5e3971aad54cb952080f9
Tommy Sullivan <tommy.b.sullivan@gmail.com>
2017-07-24 12:59:19 +0700

    fix tests

--- spec/features/sponsor_flow_spec.rb
+++ spec/features/sponsor_flow_spec.rb
@@ -5,7 +5,7 @@ feature "Sponsorship Flow" do
   scenario "Visitors can navigate to the Sponsorship page" do
     visit home_path
     click_link "Sponsor"
-    expect(page.current_path).to eq(sponsor_path)
+    expect(page.current_url).to include(home_path(anchor: "organizers"))
   end
 
 
--- spec/features/ticket_flow_spec.rb
+++ spec/features/ticket_flow_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature "Ticket Flow" do
 
   scenario "Visitors can buy a ticket" do
+    pending "Next Year!"
     visit home_path
     click_link "Get Tickets", match: :first
     expect(page.current_path).to eq(ticket_path)
